1e8e150cdf355c7125b967538df0e6d4cc85bc3c
Cai Hui <cai.hui@zte.com.cn>
2019-01-29 02:39:27 -0500

    Add Maze class and draw grphic

--- algorithm/migong.py
+++ algorithm/migong.py
@@ -4,14 +4,22 @@
     the func of walk() is a recursive function.
 
 '''
+from __future__ import print_function
 import copy
+import time
+import os
+
 maze_1=[
-      [1,0,1,1,1,1],
-      [1,1,1,0,0,1],
-      [0,1,0,0,0,1],
-      [1,1,0,1,0,1],
-      [0,1,1,1,0,1],
-      [1,1,0,1,1,1]]
+      [1,0,1,1,1,1,0,1,1,1],
+      [1,1,0,0,0,1,0,1,0,1],
+      [0,1,0,0,1,1,1,1,0,1],
+      [1,1,0,1,1,0,0,1,1,1],
+      [1,0,0,1,1,1,1,1,1,0],
+      [1,0,0,1,0,1,0,0,0,0],
+      [1,1,0,1,0,1,0,1,1,1],
+      [0,1,0,1,0,1,0,1,0,1],
+      [0,1,1,1,0,1,0,1,0,1],
+      [1,1,0,1,0,1,1,1,0,1]]
 
 
 class Maze(object):
@@ -36,11 +44,16 @@ class Maze(object):
 
         if(x == self.end_x and y == self.end_y):
             self.maze[x][y]=2
-            print("successful!")
-            self.success = 1 
+            self.success = 1
+            self.graphic(x,y) 
             return True
     
-        if self.success !=1 and self._valid(x,y) :
+        if self._valid(x,y):
+            self.graphic(x,y)
+        else:
+            return False
+
+        if self.success !=1  :
             self.maze[x][y] = 2
             if ((self.walk( x-1, y)) or
                 (self.walk( x, y-1)) or
@@ -53,17 +66,43 @@ class Maze(object):
 
         return False
 
+    def graphic(self,x,y):
+        width  = len(self.maze)
+        height = len(self.maze)
+        
+        time.sleep(1)
+        os.system('clear')
+
+        print("Start Location: 0 0") 
+        print("End Location: ",self.end_x,self.end_y)
+        print("Current Loction:",x,y)
+        print()
+
+        for x in range(width):
+            print("{0:8}".format(x), end='' )
+        print('\r\n')
+      
+        for i in range(height - 1, -1, -1):
+            print("{0:4d}".format(i), end='')
+
+            for j in range(width):
+                p = self.maze[i][j]
+                if p == 1:
+                    print('_'.center(8), end='')
+                elif p == 2:
+                    print('O'.center(8), end='')
+                else:
+                    print('X'.center(8), end='')
+            print('\r\n\r\n')
 
 
 if __name__ == "__main__":
-    kwargs={}
-    kwargs['maze'] = copy.deepcopy(maze_1)
-    kwargs['end_x']= 5
-    kwargs['end_y'] =5
-    my_maze = Maze( **kwargs)
-    for i in range (len(my_maze.maze)):
-         print(my_maze.maze[i])
-    my_maze.walk(0,0)       
-    for i in range (len(my_maze.maze)):
-         print(my_maze.maze[i])
 
+    kwargs = {}
+    kwargs['maze']  = copy.deepcopy(maze_1)
+    kwargs['end_x'] = 9
+    kwargs['end_y'] = 9
+    
+    my_maze = Maze(**kwargs)    
+    my_maze.walk(0,0)
+       
