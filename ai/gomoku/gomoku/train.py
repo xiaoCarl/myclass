7f8f1d0d5525bb8c40be9bc5c55ff5e4935b951d
Caihui <caihui_nj@163.com>
2019-12-12 17:26:10 -0800

    restore train.py

--- ai/gomoku/gomoku/train.py
+++ ai/gomoku/gomoku/train.py
@@ -132,16 +132,45 @@ class Train():
         state_batch = [data[0] for data in mini_batch]
         mcts_probs_batch = [data[1] for data in mini_batch]
         winner_batch = [data[2] for data in mini_batch]
-
+        old_probs, old_v = self.policy_value_net.policy_value(state_batch)
         for i in range(self.epochs):
-            loss_p,loss_v = self.policy_value_net.train_step(
-                            state_batch,
-                            mcts_probs_batch,
-                            winner_batch,
-                            self.learn_rate)
-            print("loss_probs:{},loss_value:{}".format(loss_p,loss_v))
-        return loss_p,loss_v
+            loss, entropy = self.policy_value_net.train_step(
+                    state_batch,
+                    mcts_probs_batch,
+                    winner_batch,
+                    self.learn_rate*self.lr_multiplier)
+            new_probs, new_v = self.policy_value_net.policy_value(state_batch)
+            kl = np.mean(np.sum(old_probs * (
+                    np.log(old_probs + 1e-10) - np.log(new_probs + 1e-10)),
+                    axis=1)
+            )
+            if kl > self.kl_targ * 4:  # early stopping if D_KL diverges badly
+                break
+        # adaptively adjust the learning rate
+        if kl > self.kl_targ * 2 and self.lr_multiplier > 0.1:
+            self.lr_multiplier /= 1.5
+        elif kl < self.kl_targ / 2 and self.lr_multiplier < 10:
+            self.lr_multiplier *= 1.5
 
+        explained_var_old = (1 -
+                             np.var(np.array(winner_batch) - old_v.flatten()) /
+                             np.var(np.array(winner_batch)))
+        explained_var_new = (1 -
+                             np.var(np.array(winner_batch) - new_v.flatten()) /
+                             np.var(np.array(winner_batch)))
+        print(("kl:{:.5f},"
+               "lr_multiplier:{:.3f},"
+               "loss:{},"
+               "entropy:{},"
+               "explained_var_old:{:.3f},"
+               "explained_var_new:{:.3f}"
+               ).format(kl,
+                        self.lr_multiplier,
+                        loss,
+                        entropy,
+                        explained_var_old,
+                        explained_var_new))
+        return loss, entropy
   
     #进行评估
     def policy_evaluate(self, n_games=10):
